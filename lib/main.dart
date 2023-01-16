6b811d84600557589b284baacc515bcc432e01fd
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-01-15 21:57:08 -0300

    errinho

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
-import 'package:first_game_with_flame/open-world-scene.dart';
+import 'package:first_game_with_flame/open_world_scene.dart';
 
 void main() {
   runApp(GameWidget(game: OpenWorldScene()));
